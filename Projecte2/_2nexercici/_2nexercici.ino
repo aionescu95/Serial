3d7fa7de75967676fd3f5dd50fd144759f85f069
aionescu95 <iadrian629@gmail.com>
2015-05-08 20:09:24 +0200

    Projecte2

---
+++ Projecte2/_2nexercici/_2nexercici.ino
@@ -0,0 +1,25 @@
+int a = 3;
+int b = 4;
+
+
+void setup()              // run once, when the sketch starts
+{
+  Serial.begin(9600);     // set up Serial library at 9600 bps
+
+  Serial.println("Lets calculate a Hypoteneuse: ");
+
+  Serial.print("a = ");
+  Serial.println(a);
+  Serial.print("b = ");
+  Serial.println(b);
+ 
+
+  Serial.print("h = ");       // add
+  Serial.println(sqrt(a*a+b*b));
+
+ }
+
+void loop()  // we need this to be here even though its empty
+{
+}
+
