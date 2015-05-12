a496cd8359655ab937869e4d2ff8a148007ca273
aionescu95 <iadrian629@gmail.com>
2015-05-12 13:56:25 +0200

    Projecte3

--- Projecte3/_1rexercici.ino/_1rexercici.ino.ino
+++ Projecte3/_1rexercici.ino/_1rexercici.ino.ino
@@ -4,18 +4,24 @@ void setup()             // run once, when the sketch starts
 {
   Serial.begin(9600);    // set up Serial library at 9600 bps
 
-  Serial.println("Different formats for the same number:");
+  Serial.println("Different formats for the same number:"); 
 
   Serial.write(num);
   Serial.println();
   
+  Serial.print("Numero = ");
   Serial.println(num);
+  
+  Serial.print("Decimal = ");
   Serial.println(num,DEC);
   
+  Serial.print("Binari = ");
   Serial.println(num,BIN);
   
+  Serial.print("Hexadecimal = ");
   Serial.println(num,HEX);
   
+  Serial.print("Octal = ");
   Serial.println(num,OCT);
 }
 
